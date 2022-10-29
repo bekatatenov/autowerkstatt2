942dcd576a6279b120bcf319ba1cc540991d6d63
btatenov <btatenov@nurtelecom.kg>
2022-10-29 19:29:49 +0600

    Initial commit

---
+++ src/test/java/com/autowerkstatt/autowerkstatt/AutowerkstattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autowerkstatt.autowerkstatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutowerkstattApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
